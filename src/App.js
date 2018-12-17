dedeb9568c643780016d2b48d8038db496ed85d7
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2018-12-17 13:49:14 +0300

    implemented authentication, secured staff Route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
 
 import "./App.css";
+
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Navbar />
-          <Router path="/" exact={true} component={Home} />
-          <Router path="/staff" exact={true} component={Staff} />
-          <h1>Hello</h1>
-        </div>
+        <Security
+          issuer="https://dev-339181.oktapreview.com/oauth2/default"
+          client_id="0oaidxacvcEVd1Khw0h7"
+          redirect_uri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-339181.oktapreview.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
+          </div>
+        </Security>
       </Router>
     );
   }
